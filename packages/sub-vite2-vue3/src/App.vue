7beb1a05586a3fdb84aa896b00da279918c7b538
huangyuting <huangyuting@neotrident.com>
2022-08-10 13:36:04 +0800

    add utils

--- packages/sub-vite2-vue3/src/App.vue
+++ packages/sub-vite2-vue3/src/App.vue
@@ -2,9 +2,10 @@
   <h1 :style="{ margin: '50px' }">
     这是vue3子应用页面
   </h1>
-  <SelfButton :style="{ margin: '0 50px' }" prefix="sub-vite2-vue3" />
+  <SelfButton :style="{ margin: '0 50px' }" prefix="sub-vite2-vue3" @click="logDate" />
 </template>
 
 <script lang="ts" setup>
 import SelfButton from 'button-vite2-vue3'
+import { logDate } from '@mono-qiankun/utils'
 </script>
---
+++ utils/index.ts
@@ -0,0 +1,3 @@
+export function logDate() {
+    console.log(`%c${new Date()}`, 'font-size: 15px; color: green')
+}
